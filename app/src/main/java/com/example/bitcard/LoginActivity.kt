dc3120a000440a698aa26409e3e034789d7797c7
kolovos@home <kolobosd98@gmail.com>
2022-02-23 21:09:21 +0200

    view binding login activity

--- app/src/main/java/com/example/bitcard/LoginActivity.kt
+++ app/src/main/java/com/example/bitcard/LoginActivity.kt
@@ -2,11 +2,29 @@ package com.example.bitcard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Toast
+import com.example.bitcard.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.loginBtn.setOnClickListener { onLoginButtonClick() }
     }
+
+    private fun onLoginButtonClick(){
+        val username = binding.username.text.toString().trim()
+        binding.username.text.clear()
+        val password = binding.password.text.toString().trim()
+        binding.password.text.clear()
+        Toast.makeText(this, "Not working", Toast.LENGTH_LONG).show()
+
+    }
+
+
 }
