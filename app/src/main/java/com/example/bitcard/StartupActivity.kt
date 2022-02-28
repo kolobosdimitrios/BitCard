450492db625df0bcf913258eb1c9a0829d7d3b55
kolovos@home <kolobosd98@gmail.com>
2022-02-28 22:53:38 +0200

    light theme until i figure out the dark theme

--- app/src/main/java/com/example/bitcard/StartupActivity.kt
+++ app/src/main/java/com/example/bitcard/StartupActivity.kt
@@ -3,6 +3,7 @@ package com.example.bitcard
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.bitcard.databinding.ActivityStartupBinding
 
 private lateinit var binding: ActivityStartupBinding
@@ -14,6 +15,7 @@ class StartupActivity : AppCompatActivity() {
         binding = ActivityStartupBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         binding.createAccountButton.setOnClickListener {
             this.startActivity(Intent(applicationContext, RegisterActivity::class.java))
             finish()
