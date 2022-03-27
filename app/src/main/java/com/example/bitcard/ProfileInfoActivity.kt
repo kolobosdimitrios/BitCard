a2222efbb1c0d18a4c18faa19e5c889b416765c8
kolovos@home <kolobosd98@gmail.com>
2022-03-27 15:34:25 +0300

    profile info activity

---
+++ app/src/main/java/com/example/bitcard/ProfileInfoActivity.kt
@@ -0,0 +1,23 @@
+package com.example.bitcard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bitcard.databinding.ActivityProfileInfoBinding
+
+class ProfileInfoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileInfoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+    }
+}
