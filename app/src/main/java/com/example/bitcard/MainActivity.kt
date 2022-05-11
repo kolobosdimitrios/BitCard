32f19c1a72574a0e776cd5e3c8916d9b5fa5dbb7
kolovos@home <kolobosd98@gmail.com>
2022-05-11 20:56:13 +0300

    if user not logged out then login automatically

--- app/src/main/java/com/example/bitcard/MainActivity.kt
+++ app/src/main/java/com/example/bitcard/MainActivity.kt
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
 import com.example.bitcard.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_BitCard)
@@ -17,6 +19,14 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        auth = FirebaseAuth.getInstance()
+
+        if (auth.currentUser != null){
+            this.startActivity(Intent(applicationContext, MainScreenActivity::class.java))
+            finish()
+        }
+
         binding.createAccountButton.setOnClickListener {
             this.startActivity(Intent(applicationContext, RegisterActivity::class.java))
             finish()
