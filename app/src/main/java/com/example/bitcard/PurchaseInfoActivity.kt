1e7561fba81683b0c31842b29193e80deeac49ad
kolobosdimitrios <kolobosd98@gmail.com>
2023-01-02 17:37:56 +0200

    added PurchaseInfoActivity.kt

---
+++ app/src/main/java/com/example/bitcard/PurchaseInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bitcard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PurchaseInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_purchase_info)
+    }
+}
