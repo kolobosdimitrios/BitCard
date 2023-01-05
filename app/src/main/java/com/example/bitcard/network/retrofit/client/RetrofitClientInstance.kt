f83bb4ee5f3d95135ef615bf91244cfb4b66e354
kolobosdimitrios <kolobosd98@gmail.com>
2023-01-05 20:17:42 +0200

    changed ip address

--- app/src/main/java/com/example/bitcard/network/retrofit/client/RetrofitClientInstance.kt
+++ app/src/main/java/com/example/bitcard/network/retrofit/client/RetrofitClientInstance.kt
@@ -1,6 +1,5 @@
 package com.example.bitcard.network.retrofit.client
 
-import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -11,7 +10,7 @@ object RetrofitHelper {
     private var okHttpClient = OkHttpClient.Builder().build()
 
     fun getRetrofitInstance(): Retrofit {
-       return Retrofit.Builder().baseUrl("http://192.168.1.19:3000/api/v1/")
+       return Retrofit.Builder().baseUrl("http://192.168.1.1:3000/api/v1/")
            .addConverterFactory(GsonConverterFactory.create())
            .client(okHttpClient)
            .build()
