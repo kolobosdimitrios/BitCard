61cb5d2d0f7513178382e0ff8e3ef2ee2d9016f5
dimitrisKolovos <kolobosd98@gmail.com>
2023-03-22 20:42:52 +0200

    home fragment with view model

--- app/src/main/java/com/example/bitcard/MainActivityBottomNavigation.kt
+++ app/src/main/java/com/example/bitcard/MainActivityBottomNavigation.kt
@@ -1,18 +1,32 @@
 package com.example.bitcard
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.activityViewModels
 import com.example.bitcard.databinding.ActivityMainBottomNavigationBinding
+import com.example.bitcard.ui.home.HomeFragment
+import com.example.bitcard.ui.home.HomeFragmentViewModel
+import com.example.bitcard.ui.purchases.PurchasesFragment
+import com.example.bitcard.ui.settings.SettingsFragment
+import com.example.bitcard.ui.shops.ShopsFragment
 
 class MainActivityBottomNavigation : AppCompatActivity() {
 
 private lateinit var binding: ActivityMainBottomNavigationBinding
 
+    private val homeFragmentViewModel : HomeFragmentViewModel by viewModels() {defaultViewModelProviderFactory}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
          binding = ActivityMainBottomNavigationBinding.inflate(layoutInflater)
          setContentView(binding.root)
+
+        val homeFragment = HomeFragment()
+        val shopsFragment = ShopsFragment()
+        val purchasesFragment = PurchasesFragment()
+        val settingsFragment = SettingsFragment()
 
     }
 }
--- app/src/main/java/com/example/bitcard/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/bitcard/ui/home/HomeFragment.kt
@@ -1,6 +1,104 @@
 package com.example.bitcard.ui.home
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.core.content.ContextCompat.getColor
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.example.bitcard.R
+import com.example.bitcard.databinding.FragmentHomeBinding
+import com.example.bitcard.db.entities.Coupon
+import com.example.bitcard.db.entities.User
+import com.example.bitcard.globals.QR
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binder: FragmentHomeBinding
+    private val homeFragmentViewModel : HomeFragmentViewModel by activityViewModels() {defaultViewModelProviderFactory}
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binder = FragmentHomeBinding.inflate(inflater)
+
+        homeFragmentViewModel.selectedUser.observe(viewLifecycleOwner){
+            user -> renderLayoutWithUserData(user)
+        }
+
+        homeFragmentViewModel.selectedToken.observe(viewLifecycleOwner){ token ->
+            drawQr(binder.cardImageView, token.token)
+
+        }
+
+        homeFragmentViewModel.selectedCoupons.observe(viewLifecycleOwner){
+            coupons -> renderCouponsAndPoints(coupons)
+        }
+        return binder.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun renderCouponsAndPoints(coupons: List<Coupon>){
+
+        binder.availableCouponsTextView.text = coupons.size.toString()
+
+    }
+
+    private fun renderLayoutWithUserData(user: User){
+
+        Log.e("user data", user.toString())
+
+        val nameSurname = String.format(resources.getString(R.string.space_between), user.name, user.surname)
+
+        binder.username.text = nameSurname
+        binder.pointsTextView.text = user.points.toString()
+        binder.nextRewardTextView.text = user.remainingPoints.toString()
+        user.image?.let {
+            if(it.trim().isNotEmpty()) {
+                val bmp = decodeImageToBitmap(it)
+                binder.profilePicture.setImageBitmap(bmp)
+            }
+        }
+
+
+    }
+
+    private fun decodeImageToBitmap(encodedImage: String): Bitmap {
+        val decodedString: ByteArray = Base64.decode(encodedImage, Base64.DEFAULT)
+        return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
+    }
+
+    fun drawQr(imageView : ImageView, content: String){
+        val bmp = QR.generate(content, requireContext().getColor(R.color.primaryDarkColor), fetchPrimaryColor())
+        imageView.setImageBitmap(bmp)
+    }
+
+    private fun fetchPrimaryColor(): Int {
+        val a = TypedValue()
+        requireActivity().theme.resolveAttribute(android.R.attr.windowBackground, a, true)
+        val color : Int = if (a.isColorType) {
+            // windowBackground is a color
+            a.data
+        }else{
+            Color.MAGENTA
+        }
+
+        return color
+    }
+
+
 }
